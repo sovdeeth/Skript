81c3723ce530b1c2d09007892efe3dc8bca6bfab
bensku <bmi@iki.fi>
2019-05-10 23:15:41 +0300

    More work on list variable

--- src/main/java/ch/njol/skript/variables/ListVariable.java
+++ src/main/java/ch/njol/skript/variables/ListVariable.java
@@ -33,14 +33,14 @@ public class ListVariable {
 	private Object[] values;
 	
 	/**
-	 * This list is basically an array.
+	 * This list is basically an array. Implies {@link #isSorted}.
 	 */
 	private boolean isArray;
 	
 	/**
 	 * Whether {@link #values} is currently in order or not.
 	 */
-	private boolean sorted;
+	private boolean isSorted;
 	
 	/**
 	 * Variable names mapped to their values. This does not exit until a random
@@ -52,7 +52,8 @@ public class ListVariable {
 	public ListVariable() {
 		this.size = 0;
 		this.values = new Object[INITIAL_ARRAY_SIZE];
-		this.sorted = true; // No content, so in order
+		this.isArray = true; // Map not initially created
+		this.isSorted = true; // No content, so in order
 	}
 	
 	private void enlargeArray() {
@@ -76,16 +77,49 @@ public class ListVariable {
 	
 	@Nullable
 	public Object get(int index) {
-		if (index > size) { // Need to do map read, array is not this large
-			// Nothing
+		if (index > size) { // Value might be in map even if it is not in array
+			// Bail out of fast path
 		} else if (isArray) { // Fast path: read from array
 			return values[index];
+		} else if (size < MIN_MAP_SIZE) { // Try to find by iterating
+			for (Object o : values) {
+				assert o instanceof VariableEntry;
+				VariableEntry entry = (VariableEntry) o;
+				if (entry.getName().equals("" + index)) {
+					return entry.getValue();
+				}
+			}
+			return null;
 		}
 		
 		// Do a map read
-		createMap();
 		assert map != null;
 		return map.get("" + index);
+	}
+	
+	@Nullable
+	public Object get(String name) {
+		if (isArray) { // All names are numeric indices
+			try {
+				int index = Integer.parseInt(name);
+				return get(index);
+			} catch (NumberFormatException e) {
+				return null; // Everything in this list has numeric index
+			}
+		} else if (size < MIN_MAP_SIZE) { // Try to find the name by iterating
+			for (Object o : values) {
+				assert o instanceof VariableEntry;
+				VariableEntry entry = (VariableEntry) o;
+				if (entry.getName().equals(name)) {
+					return entry.getValue();
+				}
+			}
+			return null;
+		} else { // Look up the name from map
+			createMap();
+			assert map != null;
+			return map.get(name);
+		}
 	}
 	
 	private void createMap() {
@@ -105,14 +139,54 @@ public class ListVariable {
 		}
 	}
 	
+	public void put(int index, Object value) {
+		if (isArray && index > 0) { // Fast paths
+			if (index < size) { // Overwrite
+				values[index] = value;
+			} else { // Append
+				enlargeArray();
+				values[index] = value;
+				size++;
+			}
+		} else { // Use generic put
+			put("" + index, value);
+		}
+	}
+	
 	public void put(String name, Object value) {
-		// Put both to array and map
-		enlargeArray();
-		values[size] = new VariableEntry(name, value);
-		size++;
-		createMap();
-		assert map != null;
-		map.put(name, value);
-		sorted = false; // Array is not sorted after random put operation
+		if (map != null) { // Map exists; add only to it
+			if (map.put(name, value) == null) { // Not overwriting old value
+				enlargeArray();
+				// Also add it to end of array
+				values[size] = new VariableEntry(name, value);
+				isArray = false; // Not just numeric indices any more
+				if (((VariableEntry) values[size - 1]).compareTo(((VariableEntry) values[size])) > 0) {
+					isSorted = false; // Array is no longer in order
+				}
+				size++;
+			}
+		} else { // No map; add to array
+			if (isArray) { // Must wrap values so they remember their indices
+				for (int i = 0; i < size; i++) {
+					Object val = values[i];
+					assert val != null;
+					values[i] = new VariableEntry("" + i, val);
+				}
+			}
+			
+			// Add value to end of array
+			VariableEntry entry = new VariableEntry(name, value);
+			int comp = ((VariableEntry) values[size - 1]).compareTo(entry);
+			if (comp == 0) { // Found a duplicate name
+				values[size - 1] = entry; // Overwrite its value
+				return;
+			} else if (comp > 0) { // No obvious duplicate
+				isSorted = false; // Array is no longer in order
+			}
+			values[size] = entry;
+			size++;
+			
+			// TODO deduplication?
+		}
 	}
 }
