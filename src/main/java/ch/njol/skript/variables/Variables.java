ca161898f92ab27747da6fb7e77cd668e8ad7691
bensku <bmi@iki.fi>
2019-10-23 17:58:43 +0300

    Add stub for getting global vars

---
+++ src/main/java/ch/njol/skript/variables/Variables.java
@@ -0,0 +1,12 @@
+package ch.njol.skript.variables;
+
+
+/**
+ * Static support code for variable system.
+ */
+public class Variables {
+	
+	public GlobalVariableScope getGlobalVariables() {
+		throw new UnsupportedOperationException("not implemented yet");
+	}
+}
