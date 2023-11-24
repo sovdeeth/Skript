07cd9df90ee5f07e3f0dbc62cc29eaa4e864c88b
sovdee <10354869+sovdeeth@users.noreply.github.com>
2023-11-23 20:52:06 -0800

    Time Until

--- src/main/java/ch/njol/skript/expressions/ExprTimeSince.java
+++ src/main/java/ch/njol/skript/expressions/ExprTimeSince.java
@@ -18,44 +18,58 @@
  */
 package ch.njol.skript.expressions;
 
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
-
 import ch.njol.skript.Skript;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.util.Date;
 import ch.njol.skript.util.Timespan;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
 
-@Name("Time Since")
-@Description("The time that has passed since a date. If the given date is in the future, a value will not be returned.")
-@Examples("send \"%time since 5 minecraft days ago% has passed since 5 minecraft days ago!\" to player")
-@Since("2.5")
+@Name("Time Since/Until")
+@Description({
+	"The time since a date has passed or the time until a date will pass. will return 0 seconds.",
+	"This expression will return 0 seconds if the time since or time until would be negative, eg. if one tries to get the time since a future date."
+})
+@Examples({
+	"send \"%time since 5 minecraft days ago% has passed since 5 minecraft days ago!\" to player",
+	"send \"%time until {countdown::ends}% until the game begins!\" to player"
+})
+@Since("2.5, INSERT VERSION (time until)")
 public class ExprTimeSince extends SimplePropertyExpression<Date, Timespan> {
 
 	static {
-		Skript.registerExpression(ExprTimeSince.class, Timespan.class, ExpressionType.PROPERTY, "[the] time since %dates%");
+		Skript.registerExpression(ExprTimeSince.class, Timespan.class, ExpressionType.PROPERTY,
+				"[the] time since %dates%",
+				"[the] (time [remaining]|remaining time) until %dates%");
 	}
+
+	private boolean isSince;
+
+	@Override
+	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		isSince = matchedPattern == 0;
+		return super.init(expressions, matchedPattern, isDelayed, parseResult);
+	}
+
 
 	@Override
 	@Nullable
 	public Timespan convert(Date date) {
 		Date now = Date.now();
-
-		/*
-		 * This condition returns whether the date the player is using is
-		 * before the current date, the same as the current date, or after the current date.
-		 * A value less than 0 indicates that the new date is BEFORE the current date.
-		 * A value of 0 indicates that the new date is the SAME as the current date.
-		 * A value greater than 0 indicates that the new date is AFTER the current date.
-		 */
-		if (date.compareTo(now) < 1)
+		// Ensure that we have a valid date
+		// Since should have a date in the past ( -1 or 0 )
+		// Until should have a date in the future ( 0 or 1 )
+		if (isSince ? (date.compareTo(now) < 1) : (date.compareTo(now) > -1))
 			return date.difference(now);
-		return null;
+		return new Timespan();
 	}
 
 	@Override
@@ -65,12 +79,12 @@ public class ExprTimeSince extends SimplePropertyExpression<Date, Timespan> {
 
 	@Override
 	protected String getPropertyName() {
-		return "time since";
+		return "time " + (isSince ? "since" : "until");
 	}
 
 	@Override
-	public String toString(final @Nullable Event e, final boolean debug) {
-		return "the time since " + getExpr().toString(e, debug);
+	public String toString(@Nullable Event event, boolean debug) {
+		return "the time " + (isSince ? "since " : "until ") + getExpr().toString(event, debug);
 	}
 
 }
