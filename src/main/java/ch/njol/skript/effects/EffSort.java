0d30f61403d9cd0419a025aa6789713408503241
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-09-01 19:51:51 -0400

    Add EffTransform, ExprTransform, tests, clean up ExprFilter, SecFilter, EffSort

--- src/main/java/ch/njol/skript/effects/EffSort.java
+++ src/main/java/ch/njol/skript/effects/EffSort.java
@@ -1,21 +1,3 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
@@ -30,7 +12,6 @@ import ch.njol.skript.expressions.ExprSortedList;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.InputSource;
-import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.Variable;
@@ -38,7 +19,7 @@ import ch.njol.skript.lang.parser.ParserInstance;
 import ch.njol.util.Kleenean;
 import ch.njol.util.Pair;
 import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.HashSet;
@@ -69,19 +50,15 @@ public class EffSort extends Effect implements InputSource {
 			ParserInstance.registerData(InputData.class, InputData::new);
 	}
 
-	@Nullable
-	private Expression<?> mappingExpr;
-	@Nullable
-	private String unparsedExpression;
-	private Variable<?> unsortedObjects;
+
+	private @Nullable Expression<?> mappingExpr;
+	private @UnknownNullability Variable<?> unsortedObjects;
 	private boolean descendingOrder;
 
-	private Set<ExprInput<?>> dependentInputs = new HashSet<>();
+	private final Set<ExprInput<?>> dependentInputs = new HashSet<>();
 
-	@Nullable
-	private Object currentValue;
-	@UnknownNullability
-	private String currentIndex;
+	private @Nullable Object currentValue;
+	private @UnknownNullability String currentIndex;
 
 	@Override
 	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
@@ -92,16 +69,12 @@ public class EffSort extends Effect implements InputSource {
 		unsortedObjects = (Variable<?>) expressions[0];
 		descendingOrder = parseResult.hasTag("descending");
 
+		//noinspection DuplicatedCode
 		if (!parseResult.regexes.isEmpty()) {
-			unparsedExpression = parseResult.regexes.get(0).group();
+			@Nullable String unparsedExpression = parseResult.regexes.get(0).group();
 			assert unparsedExpression != null;
-			InputData inputData = getParser().getData(InputData.class);
-			InputSource originalSource = inputData.getSource();
-			inputData.setSource(this);
-			mappingExpr = new SkriptParser(unparsedExpression, SkriptParser.PARSE_EXPRESSIONS, ParseContext.DEFAULT)
-				.parseExpression(Object.class);
-			inputData.setSource(originalSource);
-			return mappingExpr != null && mappingExpr.isSingle();
+			mappingExpr = parseExpression(unparsedExpression, getParser(), SkriptParser.PARSE_EXPRESSIONS);
+			return mappingExpr != null;
 		}
 		return true;
 	}
