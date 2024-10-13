8154bf2a5f9308cae6823a107bc212bd2b84a822
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-10-13 13:39:48 -0400

    better fix

--- src/main/java/ch/njol/skript/log/LogEntry.java
+++ src/main/java/ch/njol/skript/log/LogEntry.java
@@ -140,7 +140,6 @@ public class LogEntry {
 	}
 
 	public String toFormattedString() {
-		String message = Utils.replaceEnglishChatStyles(this.message);
 		if (level.intValue() < Level.WARNING.intValue())
 			return message;
 
