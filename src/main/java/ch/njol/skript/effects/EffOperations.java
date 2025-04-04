33829f3931a45ef51ec022d9c416f4ce8716169c
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-04-04 01:14:17 -0400

    Rewrite

--- src/main/java/ch/njol/skript/effects/EffOperations.java
+++ src/main/java/ch/njol/skript/effects/EffOperations.java
@@ -1,6 +1,8 @@
 package ch.njol.skript.effects;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.ClassInfo;
+import ch.njol.skript.config.Node;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Example;
 import ch.njol.skript.doc.Name;
@@ -9,17 +11,19 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.SyntaxStringBuilder;
+import ch.njol.skript.lang.Variable;
+import ch.njol.skript.registrations.Classes;
 import ch.njol.skript.util.LiteralUtils;
 import ch.njol.skript.util.Patterns;
+import ch.njol.skript.util.Utils;
 import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 import org.skriptlang.skript.lang.arithmetic.Arithmetics;
 import org.skriptlang.skript.lang.arithmetic.Operation;
 import org.skriptlang.skript.lang.arithmetic.Operator;
+import org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 @Name("Operations")
@@ -41,63 +45,116 @@ import java.util.function.Function;
 	multiply 1 by 2
 	""")
 @Since("INSERT VERSION")
-public class EffOperations extends Effect {
+public class EffOperations extends Effect implements SyntaxRuntimeErrorProducer {
 
 	private static final Patterns<Operator> patterns = new Patterns<>(new Object[][]{
-		{"multiply %~numbers% by %number%", Operator.MULTIPLICATION},
-		{"divide %~numbers% by %number%", Operator.DIVISION},
-		{"raise %~numbers% to [the] (power|exponent) [of] %number%", Operator.EXPONENTIATION}
+		{"multiply %~objects% by %object%", Operator.MULTIPLICATION},
+		{"divide %~objects% by %object%", Operator.DIVISION},
+		{"raise %~objects% to [the] (power|exponent) [of] %object%", Operator.EXPONENTIATION}
 	});
-
-	private static final Map<Operator, Operation<Number, Number, Number>> operations = new HashMap<>();
 
 	static {
 		Skript.registerEffect(EffOperations.class, patterns.getPatterns());
 	}
 
 	private Operator operator;
-	private Expression<Number> baseExpr;
-	private Expression<Number> operativeExpr;
+	private Expression<?> base;
+	private Expression<?> operative;
+	private Node node;
 
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		operator = patterns.getInfo(matchedPattern);
-		//noinspection unchecked
-		baseExpr = (Expression<Number>) exprs[0];
-		operativeExpr = LiteralUtils.defendExpression(exprs[1]);
-		return true;
+		base = exprs[0];
+		if (!(base instanceof Variable<?>)) {
+			Skript.error("Cannot operate on a non variable object.");
+			return false;
+		}
+		operative = LiteralUtils.defendExpression(exprs[1]);
+		node = getParser().getNode();
+		return LiteralUtils.canInitSafely(operative);
 	}
 
 	@Override
 	protected void execute(Event event) {
-		Number operativeNumber = operativeExpr.getSingle(event);
-		assert operativeNumber != null;
-		Operation<Number, Number, Number> operation = getOperation(operator);
-		assert operation != null;
-		Function<Number, Number> function = number -> operation.calculate(number, operativeNumber);
-		baseExpr.changeInPlace(event, function);
+		Object operativeObject = operative.getSingle(event);
+		if (operativeObject == null)
+			return;
+		ClassInfo<?> operativeClassInfo = Classes.getSuperClassInfo(operativeObject.getClass());
+		Class<?> operativeClass = operativeClassInfo.getC();
+		Operation<Object, Object, Object> operation = null;
+		Function<?, Object> function = null;
+		if (base.isSingle()) {
+			// If the variable provided is single, then we can do some checks
+			// to ensure an operation is available; if not, we can produce a proper error.
+			Object baseObject = base.getSingle(event);
+			if (baseObject == null)
+				return;
+			ClassInfo<?> baseClassInfo = Classes.getSuperClassInfo(baseObject.getClass());
+			Class<?> baseClass = baseClassInfo.getC();
+			if (!baseClass.equals(operativeClass)) {
+				operation = getOperation(baseClass, operativeClass);
+				if (operation == null) {
+					error(Utils.A(baseClassInfo.getCodeName()) + "cannot be " + getOperatorString()
+							+ " with " + Utils.a(operativeClassInfo.getCodeName()));
+					return;
+				}
+			} else {
+				operation = getOperation(operativeClass);
+				if (operation == null) {
+					error(Utils.A(operativeClassInfo.getCodeName()) + " cannot be " + getOperatorString() + ".");
+					return;
+				}
+			}
+			Operation<Object, Object, Object> finalOperation = operation;
+			function = object -> finalOperation.calculate(object, operativeObject);
+		} else {
+			// In the case of a list variable, we should probably ignore throwing multiple errors for each object
+			// that is not applicable for an operation
+			function = object -> {
+				Operation<Object, Object, Object> thisOperation = getOperation(object.getClass(), operativeClass);
+				if (thisOperation != null)
+					return thisOperation.calculate(object, operativeObject);
+				return object;
+			};
+		}
+		//noinspection unchecked,rawtypes
+		base.changeInPlace(event, (Function) function);
 	}
 
-	private static Operation<Number, Number, Number> getOperation(Operator operator) {
-		if (operations.containsKey(operator))
-			return operations.get(operator);
-		Operation<Number, Number, Number> operation = Arithmetics.getOperation(operator, Number.class, Number.class, Number.class);
-		if (operation != null) {
-			operations.put(operator, operation);
-			return operation;
-		}
-		throw new IllegalStateException("No number operation for operator '" + operator + "'.");
+	private @Nullable Operation<Object, Object, Object> getOperation(Class<?> type) {
+		//noinspection unchecked
+		return (Operation<Object, Object, Object>) Arithmetics.getConvertedOperation(operator, type, type, type);
+	}
+
+	private @Nullable Operation<Object, Object, Object> getOperation(Class<?> leftClass, Class<?> rightClass) {
+		//noinspection unchecked
+		return (Operation<Object, Object, Object>) Arithmetics.getConvertedOperation(operator, leftClass, rightClass, leftClass);
+	}
+
+	@Override
+	public Node getNode() {
+		return node;
+	}
+
+	private String getOperatorString() {
+		return switch (operator) {
+			case MULTIPLICATION -> "multiplied";
+			case DIVISION -> "divided";
+			case EXPONENTIATION -> "exponentiated";
+			default -> "";
+		};
 	}
 
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
 		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
 		switch (operator) {
-			case MULTIPLICATION -> builder.append("multiply", baseExpr, "by");
-			case DIVISION -> builder.append("divide", baseExpr, "by");
-			case EXPONENTIATION -> builder.append("raise", baseExpr, "to the power of");
+			case MULTIPLICATION -> builder.append("multiply", base, "by");
+			case DIVISION -> builder.append("divide", base, "by");
+			case EXPONENTIATION -> builder.append("raise", base, "to the power of");
 		}
-		builder.append(operativeExpr);
+		builder.append(operative);
 		return builder.toString();
 	}
 
--- src/main/java/org/skriptlang/skript/lang/arithmetic/Arithmetics.java
+++ src/main/java/org/skriptlang/skript/lang/arithmetic/Arithmetics.java
@@ -123,6 +123,60 @@ public final class Arithmetics {
 		return info == null ? null : info.getOperation();
 	}
 
+	/**
+	 * @see #getConvertedOperation(Operator, Class, Class, Class)
+	 */
+	public static <L> @Nullable Operation<L, L, L> getConvertedOperation(Operator operator, Class<?> type) {
+		//noinspection unchecked
+		return (Operation<L, L, L>) getConvertedOperation(operator, type, type, type);
+	}
+
+	/**
+	 * @see #getConvertedOperation(Operator, Class, Class, Class)
+	 */
+	public static <L, R> @Nullable Operation<L, R, ?> getConvertedOperation(Operator operator, Class<L> leftClass, Class<R> rightClass) {
+		return getConvertedOperation(operator, leftClass, rightClass, leftClass);
+	}
+
+	/**
+	 * <p>
+	 *     This method attempts to convert all parameters
+	 *     i.e. {@code leftClass}, {@code rightClass} and {@code returnType}
+	 *     into the expected operation.
+	 * </p>
+	 */
+	public static <L, R, T> @Nullable Operation<L, R, T> getConvertedOperation(
+		Operator operator,
+		Class<L> leftClass,
+		Class<R> rightClass,
+		Class<T> returnType
+	) {
+		OperationInfo<L, R, T> exactInfo = getOperationInfo(operator, leftClass, rightClass, returnType);
+		if (exactInfo != null)
+			return exactInfo.getOperation();
+		OperationInfo<?, ?, ?> closestInfo = null;
+		for (OperationInfo<?, ?, ?> info : getOperations(operator)) {
+			if (info.getLeft().isAssignableFrom(leftClass)
+				&& info.getRight().isAssignableFrom(rightClass)
+				&& info.getReturnType().isAssignableFrom(returnType)
+			) {
+				if (closestInfo == null || (
+					closestInfo.getLeft().isAssignableFrom(info.getLeft())
+						&& closestInfo.getRight().isAssignableFrom(info.getRight())
+						&& closestInfo.getReturnType().isAssignableFrom(info.getReturnType()))
+				) {
+					closestInfo = info;
+				}
+			}
+		}
+		if (closestInfo == null)
+			return null;
+		//noinspection unchecked
+		Operation<L, R, T> convertedOperation = (Operation<L, R, T>) closestInfo.getOperation();
+		getOperations_i(operator).add(new OperationInfo<>(leftClass, rightClass, returnType, convertedOperation));
+		return convertedOperation;
+	}
+
 	@Nullable
 	public static <L, R, T> OperationInfo<L, R, T> lookupOperationInfo(Operator operator, Class<L> leftClass, Class<R> rightClass, Class<T> returnType) {
 		OperationInfo<L, R, ?> info = lookupOperationInfo(operator, leftClass, rightClass);
