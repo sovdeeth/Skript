cf7fdb66106a8a32e41c89943b98d9a4e9425bb0
sovdee <10354869+sovdeeth@users.noreply.github.com>
2025-04-30 18:17:12 -0400

    slight change to contract wording

--- src/main/java/org/skriptlang/skript/lang/simplification/Simplifiable.java
+++ src/main/java/org/skriptlang/skript/lang/simplification/Simplifiable.java
@@ -16,7 +16,7 @@ public interface Simplifiable<S extends SyntaxElement> {
 	 * An addition indicating that the value was simplified can be added in the debug string. See {@link SimplifiedLiteral}
 	 * for an example.
 	 * <br>
-	 * Simplification should never widen contracts. For example, any simplified expression should take care to return
+	 * Simplification should never invalidate contracts. For example, any simplified expression should take care to return
 	 * the same or a more specific type than the original expression, never a more generic type. Likewise, be sure to
 	 * maintain the behavior of change() and acceptsChange(). Failure to do so can result in unexpected behavior and
 	 * tricky bugs.
