753f8f2a155f02491bf287a38fef8df0dad7cb53
sovdee <10354869+sovdeeth@users.noreply.github.com>
2025-05-01 10:34:56 -0400

    Update StructUsing.java

--- src/main/java/ch/njol/skript/structures/StructUsing.java
+++ src/main/java/ch/njol/skript/structures/StructUsing.java
@@ -17,7 +17,11 @@ import org.skriptlang.skript.lang.structure.Structure;
 @Name("Using Experimental Feature")
 @Description({
 	"Place at the top of a script file to enable an optional experimental feature.",
-	"Experimental features may change behavior in Skript and may contain bugs. Use at your own discretion."
+	"Experimental features may change behavior in Skript and may contain bugs. Use at your own discretion.",
+	"A non-exhaustive list of experiments as of INSERT VERSION:",
+	"- <code>using queues</code>: Enables experimental queue (FIFO).",
+	"- <code>using for loops</code>: Enables for loops.",
+	"- <code>using script reflection</code>: Enables script reflection, allowing dynamic function calls and more.",
 })
 @Examples({
 	"using 1.21",
