51525458196d0c2f5dd56d63c23d89c75dd33037
sovdee <10354869+sovdeeth@users.noreply.github.com>
2025-06-14 12:22:47 -0700

    allow greater than and lesser than or equal to

--- src/main/java/ch/njol/skript/conditions/CondIsEnchanted.java
+++ src/main/java/ch/njol/skript/conditions/CondIsEnchanted.java
@@ -16,26 +16,43 @@ import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Is Enchanted")
-@Description("Checks whether an item is enchanted. Enchants must match the exact level by default, unless 'or better' is used.")
+@Description("Checks whether an item is enchanted. Enchants must match the exact level by default, unless 'or better' or 'or worse' are used.")
 @Example("tool of the player is enchanted with efficiency 2")
 @Example("if player's helmet or player's boots are enchanted with protection 3 or better:")
+@Example("if player's chestplate is enchanted with protection")
 @Since("1.4.6, INSERT VERSION ('or better')")
 public class CondIsEnchanted extends Condition {
-	
+
+	private enum Comparison {
+		EXACT(""),
+		AT_LEAST(" or better"),
+		AT_MOST(" or worse");
+
+		private final String toString;
+
+		Comparison(String toString) {
+			this.toString = toString;
+		}
+
+		public String toSkriptString() {
+			return toString;
+		}
+	}
+
 	static {
-		PropertyCondition.register(CondIsEnchanted.class, "enchanted [with %-enchantmenttypes% [atleast:or (better|greater)]]", "itemtypes");
+		PropertyCondition.register(CondIsEnchanted.class, "enchanted [with %-enchantmenttypes% [or (1:(better|greater|higher|above)|2:(worse|lesser|lower|below))]]", "itemtypes");
 	}
 
 	private Expression<ItemType> items;
 	private @Nullable Expression<EnchantmentType> enchs;
-	private boolean exact;
+	private Comparison comparison;
 	
 	@Override
 	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		items = (Expression<ItemType>) exprs[0];
 		enchs = (Expression<EnchantmentType>) exprs[1];
-		exact = !parseResult.hasTag("atleast");
+		comparison = Comparison.values()[parseResult.mark];
 		setNegated(matchedPattern == 1);
 		return true;
 	}
@@ -45,10 +62,11 @@ public class CondIsEnchanted extends Condition {
 		if (enchs != null) {
 			EnchantmentType[] enchantments = enchs.getAll(event);
 			boolean and = enchs.getAnd();
-			return items.check(event, item ->
-				exact ? item.hasExactEnchantments(and, enchantments)
-					: item.hasEnchantments(and, enchantments),
-				isNegated());
+			return items.check(event, item -> switch (comparison) {
+				case EXACT -> item.hasExactEnchantments(and, enchantments);
+				case AT_MOST -> item.hasEnchantmentsOrWorse(and, enchantments);
+				case AT_LEAST -> item.hasEnchantmentsOrBetter(and, enchantments);
+			}, isNegated());
 		} else {
 			return items.check(event, ItemType::hasEnchantments, isNegated());
 		}
@@ -58,7 +76,7 @@ public class CondIsEnchanted extends Condition {
 	public String toString(@Nullable Event event, boolean debug) {
 		return PropertyCondition.toString(this, PropertyType.BE, event, debug, items,
 				"enchanted" + (enchs == null ? "" : " with " + enchs.toString(event, debug)) +
-					(exact ? "" : " or better"));
+				comparison.toSkriptString());
 	}
 	
 }
